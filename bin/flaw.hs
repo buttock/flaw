7d1c4afc4d24a86aedf2c71f9de8b37c1ccc7d05
Daniel B. Giffin <dbg@scs.stanford.edu>
2010-12-29 22:21:06 -0800

    wip compiles

--- bin/flaw.hs
+++ bin/flaw.hs
@@ -39,36 +39,12 @@ main :: IO ()
 main = do
   time <- getCurrentTime
   stateM <- newMVar $ initSt { stateKey = fromIntegral $ fromEnum $ utctDayTime time }
-  -- let route = routeReqM stateM systemRoute
   server 8000 systemRoute (runReqM stateM)
 
 runReqM :: MVar St -> ReqM a -> IO a
-runReqM stateM m = modifyMVar stateM $ fmap swap . runStateT m
-{-
-  modifyMVar stateM $ \state -> do
+runReqM stateM m = modifyMVar stateM $ \state -> do
     (x, state') <- runStateT m state
     return (state', x)
--}
-
-swap :: (a,b) -> (b,a)
-swap (x,y) = (y,x)
-
-{-
-routeReqM :: MVar St -> HttpRoute ReqM -> HttpRoute IO
-routeReqM stateM route =
-  HttpRoute $
-    fmap (runReqMI stateM (runResp $ runReqM stateM)) .
-      runHttpRoute route
-
-runResp :: HttpResp ReqM -> HttpResp IO
-
-adaptReqMI :: St -> (a -> b) -> Iter t ReqM a -> Iter t IO b
-adaptReqMI stateM adaptResult iter = adaptIter adaptResult adaptComputation iter
- where
-  adaptComputation m = do
-    x <- runReqM stateM m
-    runReqMI 
--}
 
 --
 -- Game state
@@ -140,7 +116,13 @@ initSt = St { stateGames = Map.empty
 
 type ReqM = StateT St IO
 
-type RouteFn m = HttpReq -> Iter L m (HttpResp  m)
+type ReqI a = Iter L ReqM a
+type ReqIResp = ReqI (HttpResp ReqM)
+
+type RouteFn = HttpReq -> ReqIResp
+
+getSt :: ReqI St
+getSt = lift $ get
 
 --
 -- Request handler
@@ -161,257 +143,28 @@ gamesRoute =
           -- , routeVar $ gameRoute
           ]
 
-showGames :: RouteFn ReqM
-showGames = do
+showGames :: RouteFn
+showGames req = do
+  s <- getSt
+  links <- mapM gameLink $ Map.elems (stateGames s)
   ok $ page "Games" $ thediv <<
     [ h2 << "Games"
-    , ulist << map ((li <<) . gameLink s) (Map.elems $ stateGames s)
+    , ulist << map (li <<) links
     , thediv << form ! [ action "/games", method "POST" ] <<
         [ submit "start" "Start Game" ]
     ]
 
-gameLink :: Game -> ReqM HotLink
-gameLink g = hotlink (gameUrl g) $ toHtml $ show g
+gameLink :: Game -> ReqI HotLink
+gameLink g = do
+  url <- gameUrl g
+  return $ hotlink url (toHtml $ show g)
 
-gameUrl :: Game -> ReqM URL
-gameUrl g = getSt >>= \s -> do
+gameUrl :: Game -> ReqI URL
+gameUrl g = do
+  s <- getSt
   let (GameId i) = gameId g
       i' :: Integer = fromIntegral $ AES.encrypt (stateKey s) $ fromIntegral i
   return $ "/games/" ++ Base32.encode i'
-
----
---- Utils
----
- 
-ok :: Html -> R
-{-
-gameRoute g path isDealer =
-    case path of
-      [] -> showGame g isDealer
-      (x:restPath) ->
-        case S.unpack x of
-          "events" -> 
-            case S.unpack $ reqMethod req of
-              "GET" -> getEvents g restPath
-              "POST" -> postEvents g
-              _ -> badRequest "Inappropriate method"
-          _ -> notFound'
-
-
-      (is:restPath) -> do
-        s <- getState
-        let ii' :: Integer = fromMaybe 0 $ Base32.decode $ S.unpack is
-            i' :: Word128 = fromIntegral ii'
-            i :: Integer = fromIntegral $ AES.decrypt (stateKey s) i'
-        let gi = GameId i
-        case Map.lookup gi (stateGames s) of
-          Nothing -> notFound "No such game"
-          Just game -> gameRequest game restPath False
-
-handleRequest :: MVar State -> IO.Handle -> IO ()
-handleRequest stateM h = enumHandle' h |$ handleRequestI stateM h
-
-handleRequestI :: (MonadIO m) => MVar State -> IO.Handle -> Iter L m ()
-handleRequestI stateM h = do
-  req <- httpreqI
-  handleRequestI' stateM h req
-
-handleRequestI' :: (MonadIO m) => MVar State -> IO.Handle -> HttpReq -> Iter L m ()
-handleRequestI' stateM h req = topRequest (reqPathLst req)
- where
-  topRequest path =
-    case path of
-      (x:restPath) ->
-        case S.unpack x of
-          "games" -> gamesRequest restPath
-          "dealer" -> dealerRequest restPath
-          "pub" -> pubRequest restPath
-          _ -> notFound'
-      _ -> notFound'
-
-  pubRequest path =
-    case S.unpack $ reqMethod req of 
-      "GET" ->
-        -- XX: an exception here should produce "500 Internal Server Error",
-        -- or "404 Not Found" if the file does not exist
-        case safeFilePath ("pub" : map S.unpack path) of
-          Nothing -> badRequest "Bad file path"
-          Just filePath -> do
-            warn $ "GET " ++ filePath
-            ok $ FileMsg mimetype'javascript filePath
-      _ -> badRequest "Method not allowed"
-
-  dealerRequest path =
-    case path of
-      (is:restPath) -> do
-        s <- getState
-        let ii' :: Integer = fromMaybe 0 $ Base32.decode $ S.unpack is
-            i' :: Word128 = fromIntegral ii'
-            i :: Integer = fromIntegral $ AES.decrypt (stateKey s + 1) i'
-        let gi = GameId i
-        case Map.lookup gi (stateGames s) of
-          Nothing -> notFound "No such game"
-          Just game -> gameRequest game restPath True
-      _ -> notFound'
-
-  gamesRequest path =
-    case path of
-      [] -> case S.unpack $ reqMethod req of 
-              "GET" -> showGames
-              "POST" -> startGame
-              _ -> badRequest "Method not allowed."
-      (is:restPath) -> do
-        s <- getState
-        let ii' :: Integer = fromMaybe 0 $ Base32.decode $ S.unpack is
-            i' :: Word128 = fromIntegral ii'
-            i :: Integer = fromIntegral $ AES.decrypt (stateKey s) i'
-        let gi = GameId i
-        case Map.lookup gi (stateGames s) of
-          Nothing -> notFound "No such game"
-          Just game -> gameRequest game restPath False
-
-  startGame = do
-    now <- io getCurrentTime
-    ch <- io $ newChan
-    (s, game) <- io $ modifyMVar stateM $ \state ->
-      let i = stateNextGameId state
-          gi = GameId i
-          game = defaultGame { gameId = gi, gameStart = now, gameEventsReady = ch }
-          games' = Map.insert gi game (stateGames state)
-          state' = state { stateNextGameId = succ i
-                         , stateGames = games'
-                         }
-      in return (state', (state', game))
-    seeOther (gameDealerUrl s game) $ page "Game created" $ toHtml $ gameDealerLink s game
-
-  gameRequest g path isDealer =
-    case path of
-      [] -> showGame g isDealer
-      (x:restPath) ->
-        case S.unpack x of
-          "events" -> 
-            case S.unpack $ reqMethod req of
-              "GET" -> getEvents g restPath
-              "POST" -> postEvents g
-              _ -> badRequest "Inappropriate method"
-          _ -> notFound'
-
-  showGame g isDealer = do
-    s <- getState
-    let url = (if isDealer then gameDealerUrl else gameUrl) s g
-    ok $ gamePage g url isDealer
-
-  showGames = do
-    s <- getState
-    ok $ page "Games" $ thediv <<
-      [ h2 << "Games"
-      , ulist << map ((li <<) . gameLink s) (Map.elems $ stateGames s)
-      , thediv << form ! [ action "/games", method "POST" ] <<
-          [ submit "start" "Start Game" ]
-      ]
-
-  getEvents g path =
-    case path of
-      [fromS] -> getEventsFrom g $ read $ S.unpack fromS
-      _ -> notFound'
-
-  getEventsFrom g n =
-    let events = gameEventsFrom n g
-    in if null events
-      then do
-        ready <- io $ dupChan (gameEventsReady g)
-        _ <- io $ readChan ready
-        getEventsFrom' g n  
-      else
-        sendEvents events
-
-  getEventsFrom' g n = do
-    let gi = gameId g
-    s <- getState
-    case Map.lookup gi (stateGames s) of
-      Nothing -> badRequest "Game no longer available"
-      Just g' -> getEventsFrom g' n
-
-  sendEvents events =
-    ok $ JSONMessage $ showJSON events
-
-  postEvents g = do
-    -- io $ threadDelay $ 2 * 1000 * 1000
-    eventsS <- netstringI
-    let events = either error id $ resultToEither $ decode $
-                   U.toString eventsS
-    let gi = gameId g
-    gameM <- io $ modifyMVar stateM $ \s ->
-      case Map.lookup gi (stateGames s) of
-        Nothing -> return (s, Nothing)
-        Just g' -> do
-          -- now <- getCurrentTime
-          let g'' = g' { gameEvents = events ++ gameEvents g'
-                       , gameNEvents = length events + gameNEvents g'
-                       }
-              s' = s { stateGames = Map.insert gi g'' $ stateGames s }
-          return $ (s', Just g'')
-    case gameM of
-      Nothing -> badRequest "Game no longer available"
-      Just game -> do
-        io $ writeChan (gameEventsReady game) ()
-        ok "OK"
-
-  gameLink :: State -> Game -> HotLink
-  gameLink s g = hotlink (gameUrl s g) $ toHtml $ show g
-
-  gameUrl :: State -> Game -> URL
-  gameUrl s g = "/games/" ++ Base32.encode i'
-   where
-    (GameId i) = gameId g
-    i' :: Integer = fromIntegral $ AES.encrypt (stateKey s) $ fromIntegral i
-
-  gameDealerLink :: State -> Game -> HotLink
-  gameDealerLink s g = hotlink (gameDealerUrl s g) $ toHtml $ show g
-
-  gameDealerUrl :: State -> Game -> URL
-  gameDealerUrl s g = "/dealer/" ++ Base32.encode i'
-   where
-    (GameId i) = gameId g
-    i' :: Integer = fromIntegral $ AES.encrypt (stateKey s + 1) $ fromIntegral i
-
-  messageI :: (Message msg, MonadIO m) => S -> [S] -> msg -> Iter L m ()
-  messageI status headers msg =
-    inumMsg status headers msg .| handleI h
-  
-  ok :: (Message msg, MonadIO m) => msg -> Iter L m ()
-  ok msg = messageI statusOK [] msg
-
-  seeOther :: (Message msg, MonadIO m) => URL -> msg -> Iter L m ()
-  seeOther url = messageI statusSeeOther [S.pack $ "Location: " ++ url]
-
-  notFound :: (Message msg, MonadIO m) => msg -> Iter L m ()
-  notFound = messageI statusNotFound []
-
-  notFound' = notFound "Not Found"
-
-  badRequest :: (Message msg, MonadIO m) => msg -> Iter L m ()
-  badRequest = messageI statusBadRequest []
-
-  getState = io $ readMVar stateM
-
-  warn s = io $ IO.hPutStrLn IO.stderr s
-
-io :: (MonadIO m) => IO a -> m a
-io x = liftIO x
--}
-
-
---
--- JSON
---
-
-data JSONMessage = JSONMessage JSValue
-
-instance Message JSONMessage where
-  msgContentType _ = mimetype'json
-  msgBytes (JSONMessage json) = U.fromString $ encodeJSValue json
-
 
 --
 -- Html
@@ -470,6 +223,9 @@ onload = strAttr "onload"
 --
 -- Utils
 --
+ 
+ok :: Html -> ReqI (HttpResp ReqM)
+ok html = return $ mkHtmlResp stat200 (U.fromString $ renderHtml html)
 
 safeFilePath :: [String] -> Maybe FilePath
 safeFilePath pp =
